4ffcb601f468efe7a8efbe79aac7a7989a620466
Rafly777 <raflyrrq16@gmail.com>
2024-04-25 14:32:52 +0700

    lopyu

---
+++ resources/views/layouts/include/sidebar.blade.php
@@ -0,0 +1,29 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link text-center">
+        <span class="brand-text font-weight-light">PurdiGaaals.</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" >
+
+                <li class="nav-item menu-open">
+                    <a href="/" class="nav-link">
+                            Home
+                    </a>
+                </li>
+                
+                    <li class="nav-item menu-open">
+                        <a href="/albums" class="nav-link">
+                            <p>My Album</p>
+                        </a>
+                    </li>
+                </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
